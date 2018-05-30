bccf3f710934239506a89dd7f360fe4e42a5fa60
Akatosh, the God of Time and Order <borjan_blizz@hotmail.com>
2018-05-30 18:01:25 +0200

    Update Index Home page

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,60 @@
+﻿using Mediaframe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mediaframe.Controllers
+{
+    public class PostsController : Controller
+    {
+        public ApplicationDbContext Database { get; set; }
+
+        public PostsController()
+        {
+            Database = new ApplicationDbContext();
+        }
+
+        public ActionResult NewComment(int id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return HttpNotFound();
+            }
+
+            var commentContent = Request.Params["comment"];
+
+            if (string.IsNullOrEmpty(commentContent))
+            {
+                return HttpNotFound();
+            }
+
+            var userId = Database.Users.FirstOrDefault(u => u.UserName == User.Identity.Name).Id;
+            var profile = Database.Profiles.FirstOrDefault(p => p.AccountId == userId);
+            var post = Database.Posts.FirstOrDefault(p => p.Id == id);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comment = new Comment()
+            {
+                UserId = profile.Id,
+                User = profile,
+                Content = commentContent,
+                DateAdded = DateTime.Now,
+                PostId = post.Id,
+                Post = post
+            };
+
+            post.Comments.Add(comment);
+
+            Database.SaveChanges();
+
+            return Json(new { comments = post.Comments.Count, commentContent = comment.Content, commentUser = comment.User.Account.Email},
+                JsonRequestBehavior.AllowGet);
+        }
+    }
+}
